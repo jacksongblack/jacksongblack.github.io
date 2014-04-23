76f9e5603e7607ec305d87042810e1550aa62c80
Jack <jack@jack.(none)>
2014-04-23 23:15:06 +0800

    修正样式冲突

--- publics/js/push_pull.js
+++ publics/js/push_pull.js
@@ -6,14 +6,14 @@ function PullOrPush(docment) {
 PullOrPush.prototype = {
     constructor: PullOrPush,
     show: function (fn) {
-        this.element.removeClass("hide")
+        this.element.removeClass("category_hide")
         if (typeof(fn) == "function") {
             fn(this.link, this.element)
         }
         ;
     },
     hide: function (fn) {
-        this.element.addClass("hide");
+        this.element.addClass("category_hide");
         if (typeof(fn) == "function") {
             fn(this.link, this.element)
         }
@@ -22,7 +22,7 @@ PullOrPush.prototype = {
     getStatus: function () {
         var str = this.element.attr("class")
         try {
-            var status = str.match("hide")
+            var status = str.match("category_hide")
         } catch (error) {
             console.log(error)
         }
@@ -69,7 +69,7 @@ function DisplayMode(){
     this.categories = new ElementDisplay(".left_category","left_category_hide","left_category")
     this.posts = new ElementDisplay("#posts","left_posts_list_hide","left_posts_list")
     this.category_switch = new ElementDisplay(".category_switch","glyphicon-arrow-right","glyphicon-arrow-left")
-    this.category = new ElementDisplay("#category","hide","")
+    this.category_code = new ElementDisplay("#category","category_hide","category")
 }
 
 DisplayMode.prototype = {
@@ -78,12 +78,13 @@ DisplayMode.prototype = {
         this.categories.hide()
         this.posts.hide()
         this.category_switch.hide()
-        this.category.hide()
+        this.category_code.hide()
+        console.log(this.category_code.hide)
     },
     openUp:function(){
         this.categories.show()
         this.posts.show()
         this.category_switch.show()
-        this.category.show()
+        this.category_code.show()
     }
 }
