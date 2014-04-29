ba8230fe5b26052628140635a1d1fcb8aa34059c
jacksongblack <songyuchao@163.com>
2014-04-29 18:34:11 +0800

    上传添加多说评论的方法

--- publics/js/duoshuo.js
+++ publics/js/duoshuo.js
@@ -3,7 +3,6 @@ function toggleDuoshuoComments(container){
     var el = document.createElement('div');//该div不需要设置class="ds-thread"
     el.setAttribute('data-thread-key', obj.attr("data-thread-key"));//必选参数
     el.setAttribute('data-url', obj.attr("data-url"));//必选参数
-//    el.setAttribute('data-author-key', '作者的本地用户ID');//可选参数
     DUOSHUO.EmbedThread(el);
     jQuery(container).append(el);
 }
