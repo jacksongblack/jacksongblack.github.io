99af320871e62b5f9bffc4f615856ab03a3e0c4b
jacksongblack <jacksongemail@163.com>
2014-08-04 14:49:54 +0800

    上传代码

--- publics/javascript/blog.js
+++ publics/javascript/blog.js
@@ -124,6 +124,7 @@ function initPage() {
         },
         formTableSubmit: function () {
             var me = this;
+            console.log("xxxxx");
             $("#search_form").submit(function (e) {
                 e.preventDefault();
                 var regArray = me.fullTextSearch($("#search_input").val())
